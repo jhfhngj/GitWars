c06388d2e942d2d96e778775359c3e4edbbd8387
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-05-17 23:19:43 -0500

    æ

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import subprocess
+import random
 if not os.path.exists("game.json"):
     with open("game.json","a") as f:
         json.dump({"battlecount":0,"battles":[],"won":0},f)
@@ -15,6 +16,35 @@ def api(do):
     return command(f"gh api --cache 1h {do}")
 def command(do):
     return subprocess.run(do.split(),capture_output=True,text=True,check=True).stdout
+def battle(youbytes:int,thembytes:int):
+    leftyou = youbytes
+    leftthem = thembytes
+    exhaustedyou = 0
+    exhaustedthem = 0
+    while not exhaustedyou > youbytes:
+        used = random.randint(10000,500000)
+        exhaustedyou += used
+        leftthem -= used
+        print("You land a blow worth",used,"bytes.")
+    print("Now it's their turn!")
+    while not exhaustedthem > thembytes:
+        used = random.randint(10000,500000)
+        exhaustedthem += used
+        leftyou -= used
+        print("BLAM! They ate",used,"bytes of yours.")
+    print("Time to tally!")
+    print("You have",leftyou,"bytes left. Don't worry if it's in the negatives! You'll just see the opponent's soon.")
+    print("And they have",leftthem,"bytes left!")
+    print("Let's see who wins...")
+    cur["battlecount"] += 1
+    if not leftyou == leftthem:
+        if leftthem>leftyou:
+            print("You lose...")
+        else:
+            print("HOORAY! You WIN!")
+            cur["won"] += 1
+    else:
+        print("It's a tie...")
 
 print("GitWars by jhfhngj")
 print("Welcome to GitWars!")
@@ -36,16 +66,33 @@ try:
             print("You have",repocount,"repos.")
             print("Whilst battling, you won",won,"times!")
             print("You currently have",sum(map(int,api(f"/users/{uname}/repos --jq '.[].size'").splitlines())),"bytes.")
+            print("Here are your battles:"," ".join(cur["battles"]))
         else:
             print("Welcome to Battle Mode.")
             print("Here you can battle all your opponents.")
             print("Who of all the GitHub land would you like to battle?")
-            out = 0
+            out = 3.14159265358
             while out != 0:
                 tobattle = input()
                 out = os.system(f"gh api users/{tobattle} --jq '.login'")
+            print("Fetching bytes...")
+            bytesa = sum(map(int,api(f"/users/{tobattle}/repos --jq '.[].size'").splitlines()))
+            print(tobattle,"has",bytesa,"bytes. Proceed? (Y/n)")
+            if input().lower() == "y":
+                print("Battling start!")
+            else:
+                print("Battle denied.")
+                break
+            battle(sum(map(int,api(f"/users/{uname}/repos --jq '.[].size'").splitlines())),bytesa)
+            cur["battles"].append("You vs "+tobattle+"!")
+            with open("game.json","w") as f:
+                json.dump(cur,f)
             
 except (EOFError, KeyboardInterrupt):
+    with open("game.json","w") as f:
+        json.dump(cur,f)
     print("Goodbye!")
 except Exception as e:
     print("A fatal exception in GitWars has occurred:",e)
+    with open("game.json","w") as f:
+        json.dump(cur,f)
